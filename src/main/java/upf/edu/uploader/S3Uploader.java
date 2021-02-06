c3358ec5378d9a04bb8cfda69e077da01600441f
¨Xavi¨ <xavier.gallardo01@estudiant.upf.edu>
2021-02-06 18:39:57 +0100

    S3Uploader small changes

--- src/main/java/upf/edu/uploader/S3Uploader.java
+++ src/main/java/upf/edu/uploader/S3Uploader.java
@@ -2,6 +2,8 @@ package upf.edu.uploader;
 
 import java.util.List;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
@@ -12,22 +14,33 @@ public class S3Uploader implements Uploader{
 	private String credentials_profile_name;
 	
 	public S3Uploader(String _bucketName, String _prefix, String _credentials_profile_name) {
+		
 		this.bucketName = _bucketName;
 		this.prefix = _prefix;
+		//Guardar les _credentials com a tipus AWSCredentialsi no com a String
 		this.credentials_profile_name = _credentials_profile_name;
 	}
 	
 
 	public void upload(List<String> files) {
 		
-		/*try {
-			AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-					.withRegion(~~~)
-					.build();
-			
-			
-		}
-		*/
+		/*
+		 * Creating client connection 
+		 */
+		
+		AWSCredentials credentials = new BasicAWSCredentials(
+				"<AWS accesskey>",
+				"<AWS secretkey>"
+				);
+		
+		//Bucket name es el que tenim guardat com a _bucket, el path es el nostre _prefix i el File("path") es la ruta completa cap al fitxer q hem de pujar
+		AmazonS3 s3client = AmazonS3ClientBuilder
+				.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(credentials))
+				.withRegion(Regions.EU_CENTRAL_1)
+				.build();
+		
+		s3client.putObject(<bucketname>, "path", new File("path"));
 		
 	}
 	
