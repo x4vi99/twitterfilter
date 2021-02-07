0879ffa4fb39e8aad75994d2a69e8f5393300635
Gabriel Graells Solé <36732563+GabrielGraells@users.noreply.github.com>
2021-02-07 14:05:26 +0100

    Update TwitterFilter.java

--- src/main/java/upf/edu/TwitterFilter.java
+++ src/main/java/upf/edu/TwitterFilter.java
@@ -10,20 +10,28 @@ import java.util.List;
 
 public class TwitterFilter {
 	public static void main(String[] args) throws IOException {
+		long startTime = System.currentTimeMillis();
+		
 		List<String> argsList = Arrays.asList(args);
 		String language = argsList.get(0);
 		String outputFile = argsList.get(1);
 		String bucket = argsList.get(2);
+		int num_tweets = 0;
+		
 		System.out
 				.println("Language: " + language + ". Output file: " + outputFile + ". Destination bucket: " + bucket);
 		for (String inputFile : argsList.subList(3, argsList.size())) {
 			System.out.println("Processing: " + inputFile);
 			final FileLanguageFilter filter = new FileLanguageFilter(inputFile, outputFile);
-			filter.filterLanguage(language);
+			num_tweets += filter.filterLanguage(language);
 		}
-
+		System.out.println("There is a total of: " + num_tweets + " tweets.");
 		 final S3Uploader uploader = new S3Uploader(bucket, language, "default");
 		 uploader.upload(Arrays.asList(outputFile));
+		
+		 long endTime = System.currentTimeMillis();
+		 long totalTime = endTime - startTime;
+		 System.out.println("Execution Time: " + totalTime + " ms.");
 
 	}
 }
