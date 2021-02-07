b951137aed568d1a8befc739d25bd8e399dbc731
Gabriel Graells Solé <36732563+GabrielGraells@users.noreply.github.com>
2021-02-07 14:07:54 +0100

    Update S3Uploader.java

--- src/main/java/upf/edu/uploader/S3Uploader.java
+++ src/main/java/upf/edu/uploader/S3Uploader.java
@@ -11,57 +11,42 @@ import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
+public class S3Uploader implements Uploader {
 
-public class S3Uploader implements Uploader{
-	
 	private String bucketName;
 	private String prefix;
 	private String credentials_profile_name;
-	
+
 	public S3Uploader(String _bucketName, String _prefix, String _credentials_profile_name) {
-		
+
 		this.bucketName = _bucketName;
 		this.prefix = _prefix;
-		//Guardar les _credentials com a tipus AWSCredentialsi no com a String
 		this.credentials_profile_name = _credentials_profile_name;
 	}
-	
 
 	public void upload(List<String> files) {
-		
+
 		/*
-		 * Creating client connection 
+		 * Creating client connection
 		 */
-		/*
-		AWSCredentials credentials = new BasicAWSCredentials(
-				"<AWS accesskey>",
-				"<AWS secretkey>"
-				);
-		
-		*/
 		String filename = files.get(0);
-		String filekey = this.prefix+"-"+filename;
-		
-		AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-				.withRegion(Regions.US_EAST_1)
-				.build();
-	
-		if(!s3Client.doesBucketExistV2(this.bucketName)) {	
-			 s3Client.createBucket(this.bucketName);
+		String filekey = this.prefix + "-" + filename;
+
+		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+
+		if (!s3Client.doesBucketExistV2(this.bucketName)) {
+			s3Client.createBucket(this.bucketName);
 		}
 		try {
-			PutObjectRequest request = new PutObjectRequest(this.bucketName,filekey, new File(filename));
+			PutObjectRequest request = new PutObjectRequest(this.bucketName, filekey, new File(filename));
 			ObjectMetadata metadata = new ObjectMetadata();
-	        metadata.setContentType("plain/text");
-	        request.setMetadata(metadata);
+			metadata.setContentType("plain/text");
+			request.setMetadata(metadata);
 			s3Client.putObject(request);
 		} catch (AmazonS3Exception e) {
 			System.err.println(e.getErrorMessage());
 		}
-	
-		
 
 	}
-	
-	
+
 }
